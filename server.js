28f71250b14ec5e5aa1246b8ef498f0f9a1a4f7d
Fachrurazy Atijani <atijani.neon@gmail.com>
2025-01-12 14:56:21 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,3 @@
-// const express = require('express')
-// const bodyParser = require('body-parser')
-// const dotenv = require('dotenv')
-// const userRoutes = require('./routes/userRoute')
-
-// dotenv.config()
-
-// const app = express()
-
-// app.use(bodyParser.json())
-// app.use(userRoutes)
-
-// const PORT = process.env.PORT || 3000
-
-// app.listen(PORT, () => {
-//     console.log(`Server running on http://localhost:${PORT}`)
-// })
-
-
 const express = require('express');
 const db = require('./config/database');
 const userRoutes = require('./routes/userRoute');
